6bc01bfc9ae100073acb6b7e1a90f6b958b50ca1
BarryDong <286084845@qq.com>
2023-09-17 15:09:52 +0800

    封装 getRequest 请求方法 & 首页附近店铺数据请求

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,11 +2,21 @@ import axios from 'axios'
 
 const service = axios.create({
   baseURL: 'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd',
-  timeout: 5000,
+  timeout: 10000,
   headers: { 'Content-Type': 'application/json' }
 })
 
-export const postRequest = (url, data) => {
+export const getRequest = (url, params = {}) => {
+  return new Promise((resolve, reject) => {
+    service.get(url, { params }).then(response => {
+      resolve(response.data)
+    }, error => {
+      reject(error)
+    })
+  })
+}
+
+export const postRequest = (url, data = {}) => {
   return new Promise((resolve, reject) => {
     service.post(url, data).then(response => {
       resolve(response.data)
