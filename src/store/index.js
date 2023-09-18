e87584a876b1d796c936188f1087c2676528fed5
BarryDong <286084845@qq.com>
2023-09-18 23:30:16 +0800

    购物车信息持久化存储

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,17 @@
 import { createStore } from 'vuex'
 
+const setLocalCartList = state => {
+  localStorage.setItem('cartList', JSON.stringify(state.cartList))
+}
+
+const getLocaCartList = () => {
+  // { shopId: { shopName:'', productList: { productId: {} } } }
+  return JSON.parse(localStorage.getItem('cartList')) || {}
+}
+
 export default createStore({
   state: {
-    cartList: {}
+    cartList: getLocaCartList()
   },
   getters: {},
   mutations: {
@@ -19,21 +28,25 @@ export default createStore({
       if (product.count < 0) product.count = 0
       shopInfo.productList[productId] = product
       state.cartList[shopId] = shopInfo
+      setLocalCartList(state)
     },
     changeShopName(state, payload) {
       const { shopId, shopName } = payload
       const shopInfo = state.cartList[shopId] || { shopName: '', productList: {} }
       shopInfo.shopName = shopName
       state.cartList[shopId] = shopInfo
+      setLocalCartList(state)
     },
     changeCartItemChecked(state, payload) {
       const { shopId, productId } = payload
       const product = state.cartList[shopId].productList[productId]
       product.check = !product.check
+      setLocalCartList(state)
     },
     cleanCartProducts(state, payload) {
       const { shopId } = payload
       state.cartList[shopId].productList = {}
+      setLocalCartList(state)
     },
     setCartItemsChecked(state, payload) {
       const { shopId } = payload
@@ -44,6 +57,7 @@ export default createStore({
           product.check = true
         }
       }
+      setLocalCartList(state)
     }
   },
   actions: {},
