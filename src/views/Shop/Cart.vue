701a2ea50bdd32d9dbcf3d2cd42fa3eed074696d
BarryDong <286084845@qq.com>
2023-09-18 21:35:58 +0800

    购物车样式及代码优化

--- src/views/Shop/Cart.vue
+++ src/views/Shop/Cart.vue
@@ -1,18 +1,20 @@
 <template>
-  <div v-if="showCart" class="mask"></div>
+  <div v-if="showCart" class="mask" @click="handleCartShowChange"></div>
   <div class="cart">
 
     <div v-if="showCart" class="product">
       <div class="product__header">
         <div class="product__header__all" @click="setCartItemsChecked(shopId)">
-          <span v-html="allChecked ? '&#xe652;': '&#xe6f7;'" class="product__header__icon iconfont"></span>全选
+          <span v-html="allChecked ? '&#xe652;': '&#xe667;'" class="product__header__icon iconfont"></span>全选
         </div>
-        <div class="product__header__clear" @click="cleanCartProducts(shopId)">清空购物车</div>
+        <div class="product__header__clear">
+          <span class="product__header__clear__btn" @click="cleanCartProducts(shopId)">清空购物车</span>
+        </div>
       </div>
       <template v-for="item in productList" :key="item._id">
         <div v-if="item.count > 0" class="product__item">
           <div
-            v-html="item.check ? '&#xe652;': '&#xe6f7;'"
+            v-html="item.check ? '&#xe652;': '&#xe667;'"
             class="product__item__checked iconfont"
             @click="changeCartItemChecked(shopId, item._id)"
           />
@@ -41,7 +43,9 @@
       <div class="check__info">
         总计：<span class="check__info__price">&yen; {{ price }}</span>
       </div>
-      <div class="check__btn">去结算</div>
+      <div class="check__btn">
+        <router-link to="/">去结算</router-link>
+      </div>
     </div>
 
   </div>
@@ -117,6 +121,15 @@ const useComputedCartEffect = () => {
 
   return { total, price, allChecked, productList, changeCartItemChecked, cleanCartProducts, setCartItemsChecked }
 }
+
+const useToggleCartEffect = () => {
+  const showCart = ref(false)
+  const handleCartShowChange = () => {
+    showCart.value = !showCart.value
+  }
+  return { showCart, handleCartShowChange }
+}
+
 export default {
   name: 'ShopCart',
   setup() {
@@ -124,10 +137,7 @@ export default {
     const shopId = route.params.id
     const { total, price, allChecked, productList, changeCartItemChecked, cleanCartProducts, setCartItemsChecked } = useComputedCartEffect()
     const { changeCartItemInfo } = useCartEffect()
-    const showCart = ref(false)
-    const handleCartShowChange = () => {
-      showCart.value = !showCart.value
-    }
+    const { showCart, handleCartShowChange } = useToggleCartEffect()
     return {
       shopId,
       total, price, allChecked, productList, changeCartItemChecked, cleanCartProducts, setCartItemsChecked,
@@ -150,18 +160,18 @@ export default {
 }
 
 .cart {
-  background: #FFF;
+  background: $bgColor;
   position: absolute; left: 0; right: 0; bottom: 0;
   z-index: 2;
 }
 
 .product {
   flex: 1;
-  background: #FFF;
+  background: $bgColor;
   overflow-y: scroll;
   &__header {
-    color: #333; font-size: .14rem;
-    border-bottom: 1px solid #F1F1F1;
+    color: $content-fontcolor; font-size: .14rem;
+    border-bottom: 1px solid $content-bgColor;
     line-height: .52rem;
     display: flex;
     &__all {
@@ -169,13 +179,18 @@ export default {
       margin-left: .18rem;
     }
     &__icon {
-      color: #0091FF; font-size: .2rem;
+      color: $btn-bgColor; font-size: .2rem;
       display: inline-block;
+      vertical-align: top;
+      margin-right: .1rem;
     }
     &__clear {
       text-align: right;
       flex: 1;
       margin-right: .16rem;
+      &__btn {
+        display: inline-block;
+      }
     }
   }
   &__item {
@@ -184,7 +199,7 @@ export default {
     padding: .12rem 0; margin: 0 .16rem;
     position: relative;
     &__checked {
-      color: #0091FF; font-size: .2rem;
+      color: $btn-bgColor; font-size: .2rem;
       line-height: .5rem;
       margin-right: .2rem;
     }
@@ -215,7 +230,7 @@ export default {
       margin-left: .06rem;
     }
     .product__number {
-      position: absolute; right: 0; bottom: .12rem;
+      position: absolute; right: 0; bottom: .26rem;
       &__minus, &__plus {
         display: inline-block;
         font-size: .2rem;
@@ -251,7 +266,7 @@ export default {
       margin: .12rem auto;
     }
     &__tag {
-      color: #fff; font-size: .12rem;
+      color: $bgColor; font-size: .12rem;
       min-width: .2rem; height: .2rem;
       background-color: $hightlight-fontColor;
       border-radius: .1rem;
@@ -270,10 +285,13 @@ export default {
     }
   }
   &__btn {
-    color: #FFF; font-size: .14rem;
+    color: $bgColor; font-size: .14rem;
     width: .98rem;
     background-color: #4FB0F9;
     text-align: center;
+    a {
+      color: $bgColor;
+    }
   }
 }
 </style>
